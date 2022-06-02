bd0423c041f9146d492a1ccf51019c898b0c3915
gaborhosszu <hungariantek@gmail.com>
2022-06-02 21:48:30 +0200

    input fields stay filled if value is ok but results is not

--- flask_alap/app.py
+++ flask_alap/app.py
@@ -28,48 +28,53 @@ def is_int(value):
 def index():
     return render_template("index.html")
 
-@app.route('/calc')
+@app.route('/calc', methods=["GET", "POST"])
 def calc():
     boxes_data = get_boxes()
-    szamitas = ''
-    item = None
-    # Megjönnek-e a paraméterek
-    if not request.args.get('cikkszam') and not request.args.get('suly'):
-            return render_template("calc.html", boxes=boxes_data,)
-        
-    print('Az adatok ellenőrizve!', file = sys.stderr)
-    # Súly szám-e
-    suly_str = request.args.get('suly')
-    print(request.args.get('suly'))
-    if not is_int(suly_str) and not is_float(suly_str):
-        return render_template("calc.html", boxes=boxes_data, message={"text": "Helytelen értékeket adtál meg a súlyhoz!", "category": "danger"})
-        
-    # Súly nem-negatív-e
-    suly = float(suly_str)
-    if suly < 0:
-        return render_template("calc.html", boxes=boxes_data, message={"text": "Súly nem lehet negatív!", "category": "danger"})
+    if request.method == "POST":  
+        szamitas = ''
+        item = None
+        data = dict(request.form)
+        # Megjönnek-e a paraméterek
+        if 'cikkszam' in data and 'suly' in data:
+            print('Az adatok ellenőrizve!', file = sys.stderr)
+            # Súly szám-e
+            suly_str =data['suly']
+            if not is_int(suly_str) and not is_float(suly_str):
+                data.pop("suly")
+                return render_template("calc.html", boxes=boxes_data, data=data, message={"text": "Helytelen értékeket adtál meg a súlyhoz!", "category": "danger"})
+                
+            # Súly nem-negatív-e
+            suly = float(suly_str)
+            if suly < 0:
+                data.pop("suly")
+                return render_template("calc.html", boxes=boxes_data, data=data, message={"text": "Súly nem lehet negatív!", "category": "danger"})
 
-    # Cikkszám létezik-e az adatbázisban
+            # Cikkszám létezik-e az adatbázisban
 
-    cikkszam = int(request.args.get('cikkszam'))
-    item = get_item(cikkszam)
-    if not item:
-        return render_template("calc.html", boxes=boxes_data, message={"text": "Cikkszám nem található", "category":"danger"})
-        
-    print(cikkszam, suly, file = sys.stderr)
-    print(type(cikkszam), type(suly), file = sys.stderr)
-    if box_suly:=request.args.get('box_suly'):
-        box_suly = float(box_suly)
-        print(f"Edény súlyával korrigálva {suly} -> {suly-box_suly}")
-        suly -= box_suly
+            cikkszam = int(data['cikkszam'])
+            item = get_item(cikkszam)
+            if not item:
+                data.pop("cikkszam")
+                return render_template("calc.html", boxes=boxes_data, data=data, message={"text": "Cikkszám nem található", "category":"danger"})
+                
+            print(cikkszam, suly, file = sys.stderr)
+            print(type(cikkszam), type(suly), file = sys.stderr)
+            if box_suly:=data['box_suly']:
+                box_suly = float(box_suly)
+                print(f"Edény súlyával korrigálva {suly} -> {suly-box_suly}")
+                suly -= box_suly
 
-    szamolas = suly / item["egyseg_suly"]
-    szamitas = round(szamolas,1)
-    display_txt = 'Eredmény: {} darab / méter !'.format(szamitas)
-    flash(display_txt , 'info')
-    print(item["egyseg_suly"], file = sys.stderr)
-    print(szamolas, file=sys.stderr)          
-    return render_template("calc.html", szamitas=str(szamitas), item_details=item, boxes=boxes_data)
+            szamolas = suly / item["egyseg_suly"]
+            szamitas = round(szamolas,1)
+            display_txt = 'Eredmény: {} darab / méter !'.format(szamitas)
+            flash(display_txt , 'info')
+            print(item["egyseg_suly"], file = sys.stderr)
+            print(szamolas, file=sys.stderr)          
+            return render_template("calc.html", szamitas=str(szamitas), item_details=item, boxes=boxes_data)
+    
+    if request.method == "GET":
+        return render_template("calc.html", boxes=boxes_data,)
 
 @app.route('/rolam')
 def rolam():
